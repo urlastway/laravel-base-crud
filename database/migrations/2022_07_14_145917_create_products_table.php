7e94aa173aef91fd4ef40c918334b5910c669a18
urlastway <andriy.semenchenko13@gmail.com>
2022-07-14 17:17:16 +0200

    first commit

---
+++ database/migrations/2022_07_14_145917_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('immage')->nullable();
+            $table->string('title', 50);
+            $table->text('description');
+            $table->decimal('price', 9, 3);
+            $table->string('series', 30);
+            $table->date('sale_date');
+            $table->string('type', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
