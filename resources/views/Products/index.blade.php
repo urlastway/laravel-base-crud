97856f6729d07fb6583fd0d7f9a471623b30f340
urlastway <andriy.semenchenko13@gmail.com>
2022-07-17 03:55:14 +0200

    add foreach in index.blade

--- resources/views/Products/index.blade.php
+++ resources/views/Products/index.blade.php
@@ -6,4 +6,28 @@
 
 @section('page-content')
     <h1>Tutti commics</h1>
+    <table class="table table-striped-columns">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">immage</th>
+            <th scope="col">title</th>
+            <th scope="col">type</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($products as $product)
+                <tr>
+                    <td>{{$product->id}}</td>
+                    <td>
+                        <img src="{{$product->immage}}">
+                    </td>
+                    <td>{{$product->title}}</td>
+                    <td>{{$product->type}}</td>
+                    <td></td>
+                </tr>
+            @endforeach
+        </tbody>
+      </table>
 @endsection
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -10,7 +10,7 @@
 <body>
     <div class="container">
         @yield('page-content')
-        @dump($products)
+        
     </div>
 </body>
 </html>
